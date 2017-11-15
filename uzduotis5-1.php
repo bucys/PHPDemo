9759059d79464c6ade6e91acc6e55a7e8761d873
Vytautas <bucys.vytautas@gmail.com>
2017-11-15 09:48:16 +0200

    5-1 uzduotis

---
+++ uzduotis5-1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PHP 21</title>
+    <meta $charset="UTF-8">
+</head>
+<body>
+    <?php 
+    $a = 3;
+    $b = 4;
+    $c = 5;
+    $tipas;
+    $plotas;
+    $perimetras = ($a + $b + $c) / 2;
+    
+    if ($a == round(sqrt($b * $b + $c * $c)) || $b == round(sqrt($a * $a + $c * $c)) || $c == round(sqrt($b * $b + $a * $a)) ) {
+        $tipas = 'Status';
+    }
+    
+    else if ( $a != $b && $b != $c && $a != $c && $a + $b > $c ){
+        $tipas = 'Įvairiakraštis';
+    }
+    elseif ( $a == $b && $a == $c && $b == $c){
+        $tipas = 'Lygiašonis';
+    }
+    elseif ( $a == $b && $b !== $c && $a + $b > $c + 
+        '\n' || $a == $c && $a !== $b && $a + $c > $b  + 
+        '\n' || $b == $c && $b !== $a && $b + $c > $a ){
+        $tipas = 'Lygiakraštis';
+    }
+    else {
+        $tipas = 'Joks';
+    }
+    
+    echo $tipas;
+    echo $plotas;
+    ?> 
+</body>
+
+</html>
